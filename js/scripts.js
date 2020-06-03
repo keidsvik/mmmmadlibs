49192bb139e0d1fb8d92e7ab679fa0dec346cbaa
keidsvik <61095627+keidsvik@users.noreply.github.com>
2020-06-03 12:01:11 -0700

    add errors and things to madlibs file to correct in devtools

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    event.preventDefault();
+    console.console.log();
+     (BLah blah blah)
+    const person1Input = $("input#person1").val();
+    console.log(person1Input);
+    const person2Input = $("input#person2").val();
+    const animalInput= $("input#animal").val();
+    const exclamationInput = $("input#exclamation").val();
+    const verbInput = $("input#verb").val();
+    const nounInput = $("input#noun").val();
+
+    $("person1").text(person1Input);
+    $(".person2").text(person2Input);
+    $(".animal").text(animalInput);
+    $(".exclamation").text(exclamationInput);
+    $(".verb").text(verbInput);
+    $(".noun").text(nounInput);
+
+    $("#story").show();
+  });
+});
